fda07d5d61682f92aa6fdcbb71781d802279e8ee
Lucas Sgalla <lucasgalla10@gmail.com>
2025-06-23 15:46:13 -0300

    Project created

---
+++ src/test/java/com/sgallalucas/workshop_mongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgallalucas.workshop_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
